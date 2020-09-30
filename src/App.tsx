734711a13acb67434d9fd6cd021a195d1cf820b3
Håkon Pelsholen Busterud <haakonbusterud@gmail.com>
2020-09-30 10:56:48 +0200

    Setup React Router (#8)
    Co-authored-by: giskenf <giskefreberg@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,23 @@ import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { FrontPage } from './components/FrontPage/FrontPage';
 import './index.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 //export const App: React.FC = () => <WelcomeMessage />;
 export class App extends React.Component {
   render() {
     return (
       <>
-        <Header />
-        <FrontPage />
-        <Footer />
+        <Router>
+          <Header />
+          <Switch>
+            <Route path="/test">Test test</Route>
+            <Route path="/">
+              <FrontPage />
+            </Route>
+          </Switch>
+          <Footer />
+        </Router>
       </>
     );
   }
