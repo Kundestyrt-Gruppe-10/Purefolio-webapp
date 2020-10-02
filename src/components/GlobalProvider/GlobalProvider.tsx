03e978ee883421d2bfd812c04d1ec2b32521ecc9
giskenf <giskefreberg@hotmail.com>
2020-10-02 10:34:13 +0200

    Add button

---
+++ src/components/GlobalProvider/GlobalProvider.tsx
@@ -0,0 +1,42 @@
+/* eslint no-empty-function: ["error", { "allow": ["arrowFunctions"] }] */
+/* eslint-env es6 */
+import React, { useState, createContext, ReactNode, useContext } from 'react';
+
+interface Props {
+  children: ReactNode | ReactNode[];
+}
+
+export interface QueryProviderProps {
+  searchQuery: string;
+  setSearchQuery: (searchQuery: string) => void;
+}
+
+export const GlobalContext = createContext<QueryProviderProps>({
+  searchQuery: '',
+  /*eslint-disable */
+
+  setSearchQuery: () => {}, //suppress all warnings between comments
+  
+/*eslint-enable */
+});
+
+export const GlobalProvider = ({ children }: Props): JSX.Element => {
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
+  const searchProvider: QueryProviderProps = {
+    searchQuery,
+    setSearchQuery,
+  };
+
+  /*const searchProvider = useMemo(() => ({
+        searchQuery,setSearchQuery}),[searchQuery,setSearchQuery]);*/
+
+  return (
+    <GlobalContext.Provider value={searchProvider}>
+      {' '}
+      {children}{' '}
+    </GlobalContext.Provider>
+  );
+};
+
+export const useQuery = () => useContext(GlobalContext);
