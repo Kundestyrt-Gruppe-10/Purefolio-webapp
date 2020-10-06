2d934c34fcb9ec692f7a68f46abee7020dbd61f8
Håkon Pelsholen Busterud <haakonbusterud@gmail.com>
2020-10-06 19:01:09 +0200

    Add fetch to search function

--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -2,28 +2,23 @@ import React, { useState, useEffect } from 'react';
 import Fuse from 'fuse.js';
 import { useQuery } from '../GlobalProvider/GlobalProvider';
 
-const data = [
-  { regionCode: 1, regionName: 'Norway' },
-  { regionCode: 2, regionName: 'Sweden' },
-  { regionCode: 3, regionName: 'Denmark' },
-  { regionCode: 4, regionName: 'Finland' },
-];
-
-/*fetchCountries(){
-  fetch('https://purefolio-backend-test.azurewebsites.net/regions')
-  .then(res => res.json())
-  .then(data => this.setState({data:data}));
-  console.log("Data: ", this.state.data);
-}*/
-
 export const SearchResults: React.FC = () => {
   //const [result, setResult] = useState({});
   const { searchQuery, setRegionCodes } = useQuery();
 
   // TODO: Fix api call when backend CORS settings are updated
-  /*
+
   const [hasError, setErrors] = useState(false);
-  const [regions, setRegions] = useState({});
+  const [regions, setRegions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [results, setResults] = useState([
+    {
+      regionID: 0,
+      regionCode: 'DE',
+      regionName: 'Default',
+      area: 0,
+    },
+  ]);
 
   useEffect(() => {
     async function fetchData() {
@@ -32,32 +27,43 @@ export const SearchResults: React.FC = () => {
         .json()
         .then((res) => setRegions(res))
         .catch((err) => setErrors(err));
+      setLoading(false);
     }
 
-    fetchData();
-  });
-  */
+    void fetchData();
+  }, []);
 
-  const options = {
-    keys: ['regionName'],
-  };
-  const fuse = new Fuse(data, options);
-  const res = fuse.search(searchQuery);
-  const regionResult = res.map((region) => region.item);
+  if (loading === false) {
+    const options = {
+      keys: ['regionName'],
+    };
+    const fuse = new Fuse(regions, options);
+    const res = fuse.search(searchQuery);
+    const regionResults = res.map((region) => region.item);
+    console.log(regionResults);
+  }
 
-  return (
-    <>
-      <ul>
-        {regionResult.map((result) => {
-          return (
-            <li key={result.regionCode}>
-              <button onClick={() => setRegionCodes([result.regionCode])}>
-                {result.regionName}
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-    </>
-  );
+  if (loading === true) {
+    return (
+      <>
+        <p>Loading ...</p>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <ul>
+          {results.map((result) => {
+            return (
+              <li key={result.regionCode}>
+                <button /*onClick={() => setRegionCodes([result.regionID])}*/>
+                  {result.regionName}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </>
+    );
+  }
 };
