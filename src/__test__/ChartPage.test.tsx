c9c37a4a67cbd9905ec3697582682c202fee5fed
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-10-12 15:58:40 +0200

    WIP: Add ChartPage

---
+++ src/__test__/ChartPage.test.tsx
@@ -0,0 +1,2 @@
+//const server = setupServer()
+// TODO: Set up tests
---
+++ src/pages/ChartPage/ChartPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const ChartPage: React.FC = () => {
+  // TODO: Implement
+  return <h1>TODO</h1>;
+};
