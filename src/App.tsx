e344debaeeda6161a53855168ffd4245b67e06b1
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-10-13 10:18:28 +0200

    Add simple 404 redirect and page

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,44 @@ import { FrontPage } from './pages/FrontPage/FrontPage';
 import { SearchResultsPage } from './pages/SearchResultsPage/SearchResultsPage';
 import './index.css';
 import { GlobalProvider } from './pages/GlobalProvider/GlobalProvider';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  BrowserRouter,
+  Redirect,
+} from 'react-router-dom';
+import { ChartPage } from './pages/ChartPage/ChartPage';
+import { NotFoundPage } from './pages/NotFoundPage/NoutFoundPage';
 
 export class App extends React.Component {
   render() {
     return (
       <>
         <GlobalProvider>
-          <Router>
+          <BrowserRouter>
             <HeaderComponent />
             <Switch>
+              {/* TODO: Add url params */}
+              <Route path="/chartpage/" exact>
+                <ChartPage />
+              </Route>
+              <Route path="/chartpage/:id/">
+                <ChartPage />
+              </Route>
               <Route path="/results/">
                 <SearchResultsPage />
               </Route>
-              <Route path="/">
+              <Route exact path="/">
                 <FrontPage />
               </Route>
+              <Route path="/404">
+                <NotFoundPage />
+              </Route>
+              <Redirect to="/404" />
             </Switch>
             <FooterComponent />
-          </Router>
+          </BrowserRouter>
         </GlobalProvider>
       </>
     );
