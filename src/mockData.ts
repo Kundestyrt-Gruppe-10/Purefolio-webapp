06033712f936d475d0cafccf800861d8b21b65db
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-10-13 17:33:00 +0200

    Start writing types and mockData

---
+++ src/mockData.ts
@@ -0,0 +1,430 @@
+export const naces = [
+  {
+    naceId: 1,
+    naceCode: 'A',
+    naceName: 'Agriculture, forestry and fishing',
+  },
+  {
+    naceId: 2,
+    naceCode: 'A01',
+    naceName:
+      'Crop and animal production, hunting and related service activities',
+  },
+  { naceId: 3, naceCode: 'A02', naceName: 'Forestry and logging' },
+  { naceId: 4, naceCode: 'A03', naceName: 'Fishing and aquaculture' },
+  { naceId: 5, naceCode: 'B', naceName: 'Mining and quarrying' },
+  { naceId: 6, naceCode: 'B05', naceName: 'Mining of coal and lignite' },
+  {
+    naceId: 7,
+    naceCode: 'B06',
+    naceName: 'Extraction of crude petroleum and natural gas',
+  },
+  { naceId: 8, naceCode: 'B07', naceName: 'Mining of metal ores' },
+  { naceId: 9, naceCode: 'B08', naceName: 'Other mining and quarrying' },
+  {
+    naceId: 10,
+    naceCode: 'B09',
+    naceName: 'Mining support service activities',
+  },
+  { naceId: 11, naceCode: 'C', naceName: 'Manufacturing' },
+  {
+    naceId: 12,
+    naceCode: 'C10',
+    naceName: 'Manufacture of food products',
+  },
+  { naceId: 13, naceCode: 'C11', naceName: 'Manufacture of beverages' },
+  {
+    naceId: 14,
+    naceCode: 'C12',
+    naceName: 'Manufacture of tobacco products',
+  },
+  { naceId: 15, naceCode: 'C13', naceName: 'Manufacture of textiles' },
+  {
+    naceId: 16,
+    naceCode: 'C14',
+    naceName: 'Manufacture of wearing apparel',
+  },
+  {
+    naceId: 17,
+    naceCode: 'C15',
+    naceName: 'Manufacture of leather and related products',
+  },
+  {
+    naceId: 18,
+    naceCode: 'C16',
+    naceName:
+      'Manufacture of wood and of products of wood and cork, except furniture; manufacture of articles of straw and plaiting materials',
+  },
+  {
+    naceId: 19,
+    naceCode: 'C17',
+    naceName: 'Manufacture of paper and paper product',
+  },
+  {
+    naceId: 20,
+    naceCode: 'C18',
+    naceName: 'Printing and reproduction of recorded media',
+  },
+  {
+    naceId: 21,
+    naceCode: 'C19',
+    naceName: 'Manufacture of coke and refined petroleum products',
+  },
+  {
+    naceId: 22,
+    naceCode: 'C20',
+    naceName: 'Manufacture of chemicals and chemical products',
+  },
+  {
+    naceId: 23,
+    naceCode: 'C21',
+    naceName:
+      'Manufacture of basic pharmaceutical products and pharmaceutical preparations',
+  },
+  {
+    naceId: 24,
+    naceCode: 'C22',
+    naceName: 'Manufacture of rubber and plastic products',
+  },
+  {
+    naceId: 25,
+    naceCode: 'C23',
+    naceName: 'anufacture of other non-metallic mineral products',
+  },
+  {
+    naceId: 26,
+    naceCode: 'C24',
+    naceName: 'Manufacture of basic metals',
+  },
+  {
+    naceId: 27,
+    naceCode: 'C25',
+    naceName: 'Manufacture of basic metals',
+  },
+  {
+    naceId: 28,
+    naceCode: 'C26',
+    naceName: 'Manufacture of computer, electronic and optical products',
+  },
+  {
+    naceId: 29,
+    naceCode: 'C27',
+    naceName: 'Manufacture of electrical equipment',
+  },
+  {
+    naceId: 30,
+    naceCode: 'C28',
+    naceName: 'Manufacture of machinery and equipment n.e.c.',
+  },
+  {
+    naceId: 31,
+    naceCode: 'C29',
+    naceName: 'Manufacture of motor vehicles, trailers and semi-trailers',
+  },
+  {
+    naceId: 32,
+    naceCode: 'C30',
+    naceName: 'Manufacture of other transport equipment',
+  },
+  { naceId: 33, naceCode: 'C31', naceName: 'Manufacture of furniture' },
+  { naceId: 34, naceCode: 'C32', naceName: 'Other manufacturing' },
+  {
+    naceId: 35,
+    naceCode: 'C33',
+    naceName: 'Repair and installation of machinery and equipment',
+  },
+  {
+    naceId: 36,
+    naceCode: 'D',
+    naceName: 'Electricity, gas, steam and air conditioning supply',
+  },
+  {
+    naceId: 37,
+    naceCode: 'E',
+    naceName:
+      'Water supply; sewerage, waste management and remediation activities',
+  },
+  {
+    naceId: 38,
+    naceCode: 'E36',
+    naceName: 'Water collection, treatment and supply',
+  },
+  { naceId: 39, naceCode: 'E37', naceName: 'Sewerage' },
+  {
+    naceId: 40,
+    naceCode: 'E38',
+    naceName:
+      'Waste collection, treatment and disposal activities; materials recovery',
+  },
+  {
+    naceId: 41,
+    naceCode: 'E39',
+    naceName: 'Remediation activities and other waste management services',
+  },
+  { naceId: 42, naceCode: 'F', naceName: 'Construction' },
+  { naceId: 43, naceCode: 'F41', naceName: 'Construction of buildings' },
+  { naceId: 44, naceCode: 'F42', naceName: 'Civil engineering' },
+  {
+    naceId: 45,
+    naceCode: 'F43',
+    naceName: 'Specialised construction activities',
+  },
+  {
+    naceId: 46,
+    naceCode: 'G',
+    naceName:
+      'Wholesale and retail trade; repair of motor vehicles and motorcycles',
+  },
+  {
+    naceId: 47,
+    naceCode: 'G45',
+    naceName:
+      'Wholesale and retail trade and repair of motor vehicles and motorcycles',
+  },
+  {
+    naceId: 48,
+    naceCode: 'G46',
+    naceName: 'Wholesale trade, except of motor vehicles and motorcycles',
+  },
+  {
+    naceId: 49,
+    naceCode: 'G47',
+    naceName: 'Retail trade, except of motor vehicles and motorcycles',
+  },
+  { naceId: 50, naceCode: 'H', naceName: 'Transportation and storage' },
+  {
+    naceId: 51,
+    naceCode: 'H49',
+    naceName: 'Land transport and transport via pipelines',
+  },
+  { naceId: 52, naceCode: 'H50', naceName: 'Water transport' },
+  { naceId: 53, naceCode: 'H51', naceName: 'Air transport' },
+  {
+    naceId: 54,
+    naceCode: 'H52',
+    naceName: 'Warehousing and support activities for transportation',
+  },
+  {
+    naceId: 55,
+    naceCode: 'H53',
+    naceName: 'Postal and courier activities',
+  },
+  {
+    naceId: 56,
+    naceCode: 'I',
+    naceName: 'Accommodation and food service activities',
+  },
+  { naceId: 57, naceCode: 'I55', naceName: 'Accommodation' },
+  {
+    naceId: 58,
+    naceCode: 'I56',
+    naceName: 'Food and beverage service activities',
+  },
+  {
+    naceId: 59,
+    naceCode: 'J',
+    naceName: 'Information and communication',
+  },
+  { naceId: 60, naceCode: 'J58', naceName: 'Publishing activities' },
+  {
+    naceId: 61,
+    naceCode: 'J59',
+    naceName:
+      'Motion picture, video and television programme production, sound recording and music publishing activities',
+  },
+  {
+    naceId: 62,
+    naceCode: 'J60',
+    naceName: 'Programming and broadcasting activities',
+  },
+  { naceId: 63, naceCode: 'J61', naceName: 'Telecommunications' },
+  {
+    naceId: 64,
+    naceCode: 'J62',
+    naceName: 'Computer programming, consultancy and related activities ',
+  },
+  {
+    naceId: 65,
+    naceCode: 'J63',
+    naceName: 'Information service activities',
+  },
+  {
+    naceId: 66,
+    naceCode: 'K',
+    naceName: 'Financial and insurance activities',
+  },
+  {
+    naceId: 67,
+    naceCode: 'K64',
+    naceName:
+      'Financial service activities, except insurance and pension funding',
+  },
+  {
+    naceId: 68,
+    naceCode: 'K65',
+    naceName:
+      'Insurance, reinsurance and pension funding, except compulsory social security',
+  },
+  {
+    naceId: 69,
+    naceCode: 'K66',
+    naceName:
+      'Activities auxiliary to financial services and insurance activities',
+  },
+  { naceId: 70, naceCode: 'L', naceName: 'Real estate activities' },
+  {
+    naceId: 71,
+    naceCode: 'M',
+    naceName: 'Professional, scientific and technical activities',
+  },
+  {
+    naceId: 72,
+    naceCode: 'M69',
+    naceName: 'Legal and accounting activities',
+  },
+  {
+    naceId: 73,
+    naceCode: 'M70',
+    naceName: 'Activities of head offices; management consultancy activities',
+  },
+  {
+    naceId: 74,
+    naceCode: 'M71',
+    naceName:
+      'Architectural and engineering activities; technical testing and analysis',
+  },
+  {
+    naceId: 75,
+    naceCode: 'M72',
+    naceName: 'Scientific research and development',
+  },
+  {
+    naceId: 76,
+    naceCode: 'M73',
+    naceName: 'Advertising and market research',
+  },
+  {
+    naceId: 77,
+    naceCode: 'M74',
+    naceName: 'Other professional, scientific and technical activities',
+  },
+  { naceId: 78, naceCode: 'M75', naceName: 'Veterinary activities' },
+  {
+    naceId: 79,
+    naceCode: 'N',
+    naceName: 'Administrative and support service activities',
+  },
+  {
+    naceId: 80,
+    naceCode: 'N77',
+    naceName: 'Rental and leasing activities',
+  },
+  { naceId: 81, naceCode: 'N78', naceName: 'Employment activities' },
+  {
+    naceId: 82,
+    naceCode: 'N79',
+    naceName:
+      'Travel agency, tour operator and other reservation service and related activities',
+  },
+  {
+    naceId: 83,
+    naceCode: 'N80',
+    naceName: 'Security and investigation activities',
+  },
+  {
+    naceId: 84,
+    naceCode: 'N81',
+    naceName: 'Services to buildings and landscape activities',
+  },
+  {
+    naceId: 85,
+    naceCode: 'N82',
+    naceName:
+      'Office administrative, office support and other business support activities',
+  },
+  {
+    naceId: 86,
+    naceCode: 'O',
+    naceName: 'Public administration and defence; compulsory social security',
+  },
+  { naceId: 87, naceCode: 'P', naceName: 'Education' },
+  {
+    naceId: 88,
+    naceCode: 'Q',
+    naceName: 'Human health and social work activities',
+  },
+  { naceId: 89, naceCode: 'Q86', naceName: 'Human health activities' },
+  {
+    naceId: 90,
+    naceCode: 'Q87',
+    naceName: 'Residential care activities',
+  },
+  {
+    naceId: 91,
+    naceCode: 'Q88',
+    naceName: 'Social work activities without accommodation',
+  },
+  {
+    naceId: 92,
+    naceCode: 'R',
+    naceName: 'Arts, entertainment and recreation',
+  },
+  {
+    naceId: 93,
+    naceCode: 'R90',
+    naceName: 'Creative, arts and entertainment activities',
+  },
+  {
+    naceId: 94,
+    naceCode: 'R91',
+    naceName: 'Libraries, archives, museums and other cultural activities',
+  },
+  {
+    naceId: 95,
+    naceCode: 'R92',
+    naceName: 'Gambling and betting activities',
+  },
+  {
+    naceId: 96,
+    naceCode: 'R93',
+    naceName: 'Sports activities and amusement and recreation activities',
+  },
+  { naceId: 97, naceCode: 'S', naceName: 'Other service activities' },
+  {
+    naceId: 98,
+    naceCode: 'S94',
+    naceName: 'Activities of membership organisations',
+  },
+  {
+    naceId: 99,
+    naceCode: 'S95',
+    naceName: 'Repair of computers and personal and household goods',
+  },
+  {
+    naceId: 100,
+    naceCode: 'S96',
+    naceName: 'Other personal service activities',
+  },
+  {
+    naceId: 101,
+    naceCode: 'T',
+    naceName:
+      'Activities of household as employers; undifferentiated goods- and services-producing activities of households for own account',
+  },
+  {
+    naceId: 102,
+    naceCode: 'T97',
+    naceName: 'Activities of households as employers of domestic personnel',
+  },
+  {
+    naceId: 103,
+    naceCode: 'T98',
+    naceName:
+      'Undifferentiated goods- and services-producing activities of private households for own use',
+  },
+  {
+    naceId: 104,
+    naceCode: 'U',
+    naceName: 'Activities of extraterritorial organisations and bodies',
+  },
+];
