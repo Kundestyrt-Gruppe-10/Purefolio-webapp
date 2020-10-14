b77cccdbab7f470745699fd422b1d74990303012
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-10-14 17:09:33 +0200

    Add ChartPage api calls for /naces and /regions

--- src/pages/ChartPage/ChartPage.tsx
+++ src/pages/ChartPage/ChartPage.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Redirect, useParams } from 'react-router-dom';
-import styled from 'styled-components';
-import { FooterComponent } from '../../components/FooterComponent/FooterComponent';
+import React, { useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import { Nace, Region } from '../../types';
+import { ApiGet } from '../../utils/api';
 
 interface UrlParams {
   naceRegionIdString: string;
@@ -12,15 +12,54 @@ export const ChartPage: React.FC<UrlParams> = ({
   naceRegionIdString,
   esgFactor,
 }) => {
+  // ----States----
+  const [error, setError] = useState<Error>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [regionList, setRegionList] = useState<Region[]>();
+  const [naceList, setNaceList] = useState<Nace[]>();
+
   let naceRegionIdList: number[];
-  console.log(esgFactor);
+  // Check if correct URL and parse URL string
   try {
     naceRegionIdList = naceRegionIdStringToListOrThrow404(naceRegionIdString);
   } catch (error) {
     return <Redirect to="/404" />;
   }
+
+  // Fetch data from API
+  useEffect(() => {
+    function fetchData() {
+      ApiGet<Region[]>('/regions')
+        .then((res) => setRegionList(res))
+        .catch((err) => setError(err));
+
+      ApiGet<Nace[]>('/naces')
+        .then((res) => setNaceList(res))
+        .catch((err) => setError(err));
+
+      // TODO: Fetch naceregionData
+      //naceRegionIdList.map(nace)
+
+      setLoading(false);
+    }
+
+    void fetchData();
+  }, [naceRegionIdString, esgFactor]);
+
+  // Render components
   return (
     <>
+      {loading ? (
+        <h1>loading...</h1>
+      ) : error ? (
+        /* TODO: Make error component */
+        <h1>Error: {error.message}</h1>
+      ) : (
+        <h1>
+          {regionList ? regionList[0].regionName : null}
+          {naceList ? naceList[0].naceName : null}
+        </h1>
+      )}
       {/**TODO: ChartPageHeader */}
 
       {/**TODO: NaceRegionCardContainer*/}
@@ -41,9 +80,6 @@ export const isValidNaceRegionIdString = (naceRegionIdString: string) => {
 export const naceRegionIdStringToListOrThrow404 = (
   naceRegionIdString: string,
 ): number[] => {
-  console.log('TEST');
-  console.log(naceRegionIdString);
-  console.log(isValidNaceRegionIdString(naceRegionIdString));
   if (!isValidNaceRegionIdString(naceRegionIdString)) {
     throw new Error('Illegal argument');
   }
