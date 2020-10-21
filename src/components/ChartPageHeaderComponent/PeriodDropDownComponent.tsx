1f687a928a9d84648b0dec85195201534b776523
Sindre J. I. Sivertsen <sindrejohan1@gmail.com>
2020-10-21 14:50:02 +0200

    Cus 100 create chart header (#24)
    * Add Chart Page header component file
    
    * Add OverviewTable and fix spelling error in Index.css
    
    * Change border width in Overview Table (very importantgit add src/components/OverviewTable/OverviewTable.tsx )
    
    * Create chart page header, add this to ChartPage
    
    * Add flexbox containers to header
    
    * Add new header design and new searchBar design with dropdown
    
    * Add package.json
    
    * Add small chartHeader fix
    
    * Add updated design for header and new EsgDropDown
    
    Co-authored-by: maria <mariiva@stud.ntnu.no>
    Co-authored-by: gusevAlexey96 <alexeygu@stud.ntnu.no>

--- src/components/ChartPageHeaderComponent/PeriodDropDownComponent.tsx
+++ src/components/ChartPageHeaderComponent/PeriodDropDownComponent.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
 import Select from 'react-select';
 
 const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' },
+  { value: '2018', label: '2018' },
+  { value: '2017', label: '2017' },
+  { value: '2016', label: '2016' },
 ];
 
 export const PeriodDropdown: React.FC = () => {
