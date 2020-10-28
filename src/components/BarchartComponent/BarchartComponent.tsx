d984526c28ff998e94089103a4bf4ebd3aabe0e7
maria <mariiva@stud.ntnu.no>
2020-10-28 16:52:23 +0100

    Fix background of barchart

--- src/components/BarchartComponent/BarchartComponent.tsx
+++ src/components/BarchartComponent/BarchartComponent.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import styled from 'styled-components';
 import {
+  ResponsiveContainer,
   BarChart,
   Bar,
   XAxis,
@@ -88,36 +90,150 @@ export const BarchartComponent: React.FC<Props> = ({
     });
   });
   return (
-    <>
-      <BarChart
-        width={1000}
-        height={500}
-        data={naceRegionItems}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="year" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        {naceRegionList.map((item, idx) => {
-          return (
-            // This is number of Bars per group/ year. If we compare multiple NaceRegions
-            // We will have multiple bars
-            <Bar
-              data-testid="bar"
-              key={idx}
-              dataKey={item.region.regionName + item.nace.naceCode}
-              fill={handleColorType(idx)}
-            />
-          );
-        })}
-      </BarChart>
-    </>
+    <OuterContainer active={false}>
+      <TableContainer active={false}>
+        <GraphContainer active={false}>
+          <ResponsiveContainer aspect={2.7} width="97%" height="97%">
+            <BarChart
+              width={1000}
+              height={500}
+              data={naceRegionItems}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="year" />
+              <YAxis />
+              <Tooltip />
+              {naceRegionList.map((item, idx) => {
+                return (
+                  // This is number of Bars per group/ year. If we compare multiple NaceRegions
+                  // We will have multiple bars
+                  <Bar
+                    data-testid="bar"
+                    key={idx}
+                    dataKey={item.region.regionName + item.nace.naceCode}
+                    fill={handleColorType(idx)}
+                  />
+                );
+              })}
+            </BarChart>
+          </ResponsiveContainer>
+        </GraphContainer>
+        <TextBox active={false}>
+          <TableTitleContainer active={false}>
+            <TitleBox active={false}>History Graph</TitleBox>
+            <UnitOfMeasureBox active={false}>
+              By million tonnes of CO2
+            </UnitOfMeasureBox>
+          </TableTitleContainer>
+          <TableInfoContainer active={false}>
+            <ESGFactorContainer active={false}>
+              <DescriptorBox active={false}>ESG Factor:</DescriptorBox>
+              <DescriptionBox active={false}>
+                Air Emission accounts
+              </DescriptionBox>
+            </ESGFactorContainer>
+            <PeriodContainer active={false}>
+              <DescriptorBox active={false}>Year:</DescriptorBox>
+              <DescriptionBox active={false}> 2014-2018</DescriptionBox>
+            </PeriodContainer>
+          </TableInfoContainer>
+        </TextBox>
+      </TableContainer>
+    </OuterContainer>
   );
 };
+
+const OuterContainer = styled.div<{ active: boolean }>`
+  width: 100%;
+  margin: auto;
+  padding-top: 60px;
+  padding-bottom: 10px;
+`;
+
+const TableContainer = styled.div<{ active: boolean }>`
+  background-color: var(--third-bluegrey-color);
+  width: 90%;
+  margin: auto;
+  position: relative;
+  border-radius: 5px;
+  padding-bottom: 30px;
+`;
+
+const GraphContainer = styled.div<{ active: boolean }>`
+  background-color: var(--sec-purple-color);
+  width: 90%;
+  margin: auto;
+  border-radius: 5px;
+  padding-top: 20px;
+  z-index: 1;
+  transform: translateY(-35px);
+`;
+
+const TextBox = styled.div<{ active: boolean }>`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: start;
+  margin-left: 5%;
+  margin-right: 5%;
+`;
+const TableTitleContainer = styled.div<{ active: boolean }>`
+  display: flex;
+  flex-direction: column;
+  flex-basis: 35%;
+  margin-left: 2%;
+  margin-right: 2%;
+`;
+const TitleBox = styled.div<{ active: boolean }>`
+  font-size: 20px;
+  font-weight: 700;
+  margin-right: auto;
+  text-align: center;
+  padding-top: 2px;
+  padding-bottom: 2px;
+`;
+
+const UnitOfMeasureBox = styled.div<{ active: boolean }>`
+  font-size: 14px;
+  font-weight: 100;
+  text-indent: 4%;
+`;
+
+const TableInfoContainer = styled.div<{ active: boolean }>`
+  display: flex;
+  flex-direction: column;
+  flex-basis: 35%;
+  font-weight: 450;
+  }
+`;
+
+const ESGFactorContainer = styled.div<{ active: boolean }>`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-top: 2px;
+  padding-bottom: 2px;
+`;
+
+const PeriodContainer = styled.div<{ active: boolean }>`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-top: 2px;
+  padding-bottom: 2px;
+`;
+
+const DescriptorBox = styled.div<{ active: boolean }>`
+  font-size: 14px;
+  }
+`;
+
+const DescriptionBox = styled.div<{ active: boolean }>`
+  font-size: 14px;
+`;
