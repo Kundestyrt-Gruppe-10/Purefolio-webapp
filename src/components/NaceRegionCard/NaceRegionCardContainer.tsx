10c11cfa6d2c087bac1fc7794c7a21c76b23faa5
giskenf <giskefreberg@hotmail.com>
2020-11-04 14:43:05 +0100

    Fix preset layout

--- src/components/NaceRegionCard/NaceRegionCardContainer.tsx
+++ src/components/NaceRegionCard/NaceRegionCardContainer.tsx
@@ -99,10 +99,26 @@ export const NaceRegionCardContainer: React.FC<NaceRegionContainerInterface> = (
         </Background>
         <PresetContainer>
           <h3>Presets:</h3>
-          <Link to="/chartpage/25,1;25,5;25,11/emissionPerYear/2015/2017/1">
+          <Link
+            to="/chartpage/25,1;25,5;25,11/emissionPerYear/2015/2017/1"
+            style={{
+              textDecoration: 'none',
+              backgroundColor: 'var(--third-bluegrey-color)',
+              width: '10%',
+              margin: '3px',
+            }}
+          >
             Norway: Emission
           </Link>
-          <Link to="/chartpage/2,1;1,1;13,1;7,1/fatalAccidentsAtWork/2015/2018/1">
+          <Link
+            to="/chartpage/2,1;1,1;13,1;7,1/fatalAccidentsAtWork/2015/2018/1"
+            style={{
+              textDecoration: 'none',
+              backgroundColor: 'var(--third-bluegrey-color)',
+              width: '10%',
+              margin: '3px',
+            }}
+          >
             EU: Agriculture
           </Link>
         </PresetContainer>
@@ -117,9 +133,14 @@ const Container = styled.div`
 `;
 const PresetContainer = styled.div`
   display: flex;
-  grid-column-start: 2;
+  grid-column-start: 1;
   flex-direction: column;
+  justify-content: center;
+  color: var(--sec-purple-color);
+  font-size: var(--font-size-tiny);
+  padding-left: 30px;
 `;
+
 const Background = styled.div<{ active: boolean }>`
   grid-column-start: 1;
   grid-column-end: 2;
