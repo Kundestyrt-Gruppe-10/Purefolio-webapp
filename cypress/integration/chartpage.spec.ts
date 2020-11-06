ec507806790e358877a5523b798be8ed64aced2f
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-11-06 18:48:57 +0100

    Add NaceCards tests

--- cypress/integration/chartpage.spec.ts
+++ cypress/integration/chartpage.spec.ts
@@ -1,4 +1,7 @@
 /// <reference types="cypress" />
+
+import { ClickAwayListener } from '@material-ui/core';
+
 describe('/Chartpage base layout test', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/chartpage');
@@ -26,5 +29,65 @@ describe('/ChartPage URL ', () => {
   it('Gives 404 if naceregionString contains characters', () => {
     cy.visit('http://localhost:3000/chartpage/a,1/emissionPerYear/2015/2018/1');
     cy.url().should('include', '/404');
+  });
+});
+
+// NaceRegionCards
+describe('NaceRegionCards tests', () => {
+  // Arrange
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/chartpage');
+  });
+
+  it('Can add new NaceCard', () => {
+    // Act
+    cy.get('.add-nacecard-button').click();
+    // Asses
+    cy.contains('Austria').first();
+    cy.get('.naceCardContainer').children().should('have.length', 3);
+  });
+
+  it('Can delete NaceCard', () => {
+    // Act
+    cy.get('.add-nacecard-button').click();
+    cy.get('.deleteNaceCard').first().click();
+
+    // Asses
+    cy.get('.naceCardContainer').children().should('have.length', 2);
+  });
+  it('Can change Region', () => {
+    // Act
+    cy.get('.country-select').type('Belgium{enter}');
+    // Asses
+    cy.url().should(
+      'include',
+      'http://localhost:3000/chartpage/2,1/emissionPerYear/2015/2018/1',
+    );
+    cy.contains('Belgium');
+  });
+  it('Can change Nace', () => {
+    // Act
+    cy.get('.NaceSelect').type('loggin{enter}');
+    // Asses
+    cy.url().should(
+      'include',
+      'http://localhost:3000/chartpage/1,3/emissionPerYear/2015/2018/1',
+    );
+    cy.contains('Forestry and logging');
+  });
+  it('Can duplicate NaceCard', () => {
+    // Act
+    cy.get('.country-select').type('Belgium{enter}');
+    // Act
+    cy.get('.add-nacecard-button').click();
+
+    // Asses
+    cy.url().should(
+      'include',
+      'http://localhost:3000/chartpage/2,1;2,1/emissionPerYear/2015/2018/1',
+    );
+    // Asses
+    cy.get('.naceCardContainer').children().should('have.length', 3);
+    cy.contains('Belgium');
   });
 });
