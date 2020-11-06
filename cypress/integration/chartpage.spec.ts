a27b679ee2b568346ff669e8750943ed04315617
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-11-06 16:54:26 +0100

    Add chartpage URL-tests

--- cypress/integration/chartpage.spec.ts
+++ cypress/integration/chartpage.spec.ts
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-describe('FrontPage spec', () => {
+describe('/Chartpage base layout test', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/chartpage');
   });
@@ -9,7 +9,22 @@ describe('FrontPage spec', () => {
       'This project is developed by students for Nordic Trustee, for the course TDT4290 at Norwegian University of Science and Technology.',
     );
   });
-  it('Renders Header correctly', () => {
+  it('Can find ESG Factor text in Header', () => {
     cy.contains('ESG factor');
+  });
+});
+
+// FrontPage Url
+describe('/ChartPage URL ', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/chartpage');
+  });
+  it('Redirects to default chartpage when incomplete url given', () => {
+    cy.url().should('include', 'chartpage/1,1/emissionPerYear/2015/2018/1');
+  });
+
+  it('Gives 404 if naceregionString contains characters', () => {
+    cy.visit('http://localhost:3000/chartpage/a,1/emissionPerYear/2015/2018/1');
+    cy.url().should('include', '/404');
   });
 });
