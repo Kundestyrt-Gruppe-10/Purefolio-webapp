08e87a4bdeccf2112feb2c96906d5d0be2539f31
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-11-10 16:52:35 +0100

    Fix jest SVG render bug

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,7 @@ module.exports = {
   testResultsProcessor: 'jest-sonar-reporter',
   moduleNameMapper: {
     '\\.(css)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.ts',
   },
 };
