e82b5940d5440fd09a176718c0fe2f3bb332cebe
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-11-11 16:05:45 +0100

    Add Header snapshot test

--- src/__test__/Header.test.tsx
+++ src/__test__/Header.test.tsx
@@ -1,22 +1,12 @@
-//import * as React from 'react';
-//import * as renderer from 'react-test-renderer';
-// import { HeaderComponent } from '../components/HeaderComponent/HeaderComponent';
+import React from 'react';
+import { render, RenderResult, screen } from '@testing-library/react';
+import { HeaderComponent } from '../components/HeaderComponent/HeaderComponent';
 
-it('SubjectToBeTested renders correctly', () => {
-  // TODO: jsdom does not support svg jet:
-  // This might be a solution
-  // https://github.com/svgdotjs/svg.js/issues/464#issuecomment-200551245
-  //https://stackoverflow.com/questions/44173754/jsdom-not-support-svg
-  /*
-  const props = {
-    className: 'test',
-    value: true,
-    onSelectChanged: (value: boolean) => {
-      return;
-    },
-  };
-  const tree = renderer.create(<HeaderComponent />).toJSON();
-  expect(tree).toMatchSnapshot();
-  */
-  expect(true).toBeTruthy();
+describe('<HeaderComponent />', () => {
+  beforeEach(() => {
+    render(<HeaderComponent />);
+  });
+  it('Matches snapshot', () => {
+    expect(screen).toMatchSnapshot();
+  });
 });
